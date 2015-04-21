f5d21505849bba7200683d7320066b4d4b2d37ba
theflanman <connorflanigan@gmail.com>
2015-04-21 17:25:25 -0400

    made catkin workspace

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ For simulation:
 		roslaunch turtlebot_gazebo gmapping_demo.launch
 	run RViz:
 		rosrun rviz rviz
+
+things ToDo:
+-create a navigation stack using a proper launch file
+-get move_base to cooperate
+ -test using RViz 2d nav points
+-implement exploration algorithm as explained in http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.218.8895&rep=rep1&type=pdf
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export LD_LIBRARY_PATH="/home/connor/rbe3002final/devel/lib/x86_64-linux-gnu:/opt/ros/indigo/lib/x86_64-linux-gnu:/home/connor/rbe3002final/devel/lib:/home/connor/catkin_ws/devel/lib:/opt/ros/indigo/lib:/opt/ros/indigo/lib/python2.7/dist-packages:/opt/ros/indigo/lib/python2.7/dist-packages:/opt/ros/indigo/lib/python2.7/dist-packages:/opt/ros/indigo/lib/python2.7/dist-packages"
+export PKG_CONFIG_PATH="/home/connor/rbe3002final/devel/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/x86_64-linux-gnu/pkgconfig:/home/connor/rbe3002final/devel/lib/pkgconfig:/home/connor/catkin_ws/devel/lib/pkgconfig:/opt/ros/indigo/lib/pkgconfig"
+export PWD="/home/connor/rbe3002final/build"
